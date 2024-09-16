dab4bb863932bb974e30c942e11717d38d2d2c18
Victor Cortés <cortes.victorhugo@gmail.com>
2024-09-15 22:39:24 -0500

    feat: Basic implementation of react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,65 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
 
 import "./App.css";
 
+function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cases">Support Cases</Link>
+          </li>
+          <li>
+            <Link to="/cases-detail">Detail of support cases</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </>
+  );
+}
+
 const queryClient = new QueryClient();
+
+const Home = () => <h1>Home</h1>;
+const DataDisplay = () => <h1>Data Display</h1>;
+const DataDetail = () => <h1>Data Detail</h1>;
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hello Word!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="cases" element={<DataDisplay />} />
+            <Route path="cases-detail" element={<DataDetail />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
